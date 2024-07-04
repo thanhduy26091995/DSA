46b4779cb94d157a7ffcba3c505352f548c70e0b
DESKTOP-L482ENJ\Admin <buivuthanhduy@gmail.com>
2024-07-05 00:19:17 +0700

    Chore: Linked List

---
+++ src/linkedList/LinkedList.kt
@@ -0,0 +1,65 @@
+package linkedList
+
+class LinkedList<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var size = 0
+
+    fun isEmpty(): Boolean {
+        return size == 0
+    }
+
+    override fun toString(): String {
+        return if (isEmpty()) {
+            "Empty list"
+        } else {
+            head.toString()
+        }
+    }
+
+    fun push(value: T) {
+        head = Node(value = value, next = head)
+        if (tail == null) {
+            tail = head
+        }
+        size++
+    }
+
+    fun append(value: T) {
+        if (size == 0) {
+            push(value)
+            return
+        }
+
+        tail?.next = Node(value)
+        tail = tail?.next
+    }
+
+    fun nodeAt(index: Int): Node<T>? {
+        if (index < 0 || index >= size) {
+            return null
+        }
+
+        var currentNode = head
+        var currentIndex = 0
+
+        while (currentNode != null && currentIndex < index) {
+            currentNode = currentNode.next
+            currentIndex++
+        }
+
+        return currentNode
+    }
+
+    fun insert(value: T, afterNode: Node<T>): Node<T> {
+        if (tail == afterNode) {
+            append(value)
+            return tail!!
+        }
+
+        val newNode = Node(value = value, next = afterNode.next)
+        afterNode.next = newNode
+        size++
+        return newNode
+    }
+}
---
+++ src/linkedList/Main.kt
@@ -0,0 +1,10 @@
+package linkedList
+
+fun main() {
+    val list = LinkedList<Int>()
+    list.push(1)
+    list.push(2)
+    list.push(3)
+
+    println(list)
+}
